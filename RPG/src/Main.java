704659a336cfde671d4300cabee1666d3991bd58
ReneVajda1 <rene.vajda@yahoo.com>
2024-12-26 21:50:56 +0100

    JFrame and gamepanel created

---
+++ RPG/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame(); // just jframe window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("RPG");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
