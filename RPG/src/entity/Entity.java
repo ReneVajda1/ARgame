dcf75dc588910cb048489a9df8f1e4d015ed24ba
ReneVajda1 <rene.vajda@yahoo.com>
2024-12-27 01:44:47 +0100

    Sprite-images template created - working

--- RPG/src/entity/Entity.java
+++ RPG/src/entity/Entity.java
@@ -1,6 +1,12 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
     public int x, y;
     public int speed;
+
+    public BufferedImage up1;
+    public String direction;
+
 }
--- RPG/src/entity/Player.java
+++ RPG/src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp; // Reference to GamePanel
@@ -13,23 +16,50 @@ public class Player extends Entity {
         this.gp = gp; // Initialize GamePanel reference
         this.keyH = keyH; // Initialize KeyHandler reference
         setDefaultValues(); // Set initial player values
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100; // Initial X position
         y = 100; // Initial Y position
         speed = 4; // Initial speed
+        direction = "down";
     }
-
+    // this makes SPRITE IMAGES POSSIBLE
+    public void getPlayerImage(){
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/img/hero-up.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void update() {
-        if (keyH.wPressed) y -= speed; // Move up
-        if (keyH.sPressed) y += speed; // Move down
-        if (keyH.aPressed) x -= speed; // Move left
-        if (keyH.dPressed) x += speed; // Move right
+        // Update direction and movement based on key inputs
+        if (keyH.wPressed) {
+            direction = "up"; // Set direction to up
+            y -= speed; // Move up
+        } else if (keyH.sPressed) {
+            direction = "down"; // Set direction to down
+            y += speed; // Move down
+        } else if (keyH.aPressed) {
+            direction = "left"; // Set direction to left
+            x -= speed; // Move left
+        } else if (keyH.dPressed) {
+            direction = "right"; // Set direction to right
+            x += speed; // Move right
+        }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white); // Set color for player
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize); // Draw player as rectangle
+   //     g2.setColor(Color.white); // Set color for player
+   //     g2.fillRect(x, y, gp.tileSize, gp.tileSize); // Draw player as rectangle
+        BufferedImage image = null;
+
+        switch(direction){
+            case "up":
+                image = up1;
+                break;
+        }
+        g2.drawImage(image,x,y,gp.tileSize,gp.tileSize,null);
     }
 }
